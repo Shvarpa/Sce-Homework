5eec4e2607b0d8839e5c043bda079b83ce9dde97
Pavel Shvarchov <shvarpa@gmail.com>
2017-11-06 16:40:06 +0200

    starting work

---
+++ Homework1.py
@@ -0,0 +1,48 @@
+"""
+    Home Work 1
+    Pavel Shvarchov - 319270583
+"""
+
+
+# import tkinter as tk
+
+def Nand(x, y):
+    """ 
+    :param x:
+    :type x:bool
+    :param y:
+    :type y:bool
+    :return:
+    :rtype:bool
+    """
+    return not (x and y)
+
+
+def Younger():
+    """
+    :return: younger name
+    :rtype: string
+    """
+
+    class date:
+        def __init__(self, d=None, m=None, y=None):
+            self.day = d
+            self.month = m
+            self.year = y
+
+        def __str__(self):
+            return str(self.day) + '/' + str(self.month) + '' / '' + str(self.year)
+
+        def input(self):
+            year = input("Enter year:")
+            month = input("Enter month:")
+            day = input("Enter day:")
+
+    def menu(comment):
+        """
+        :param comment:
+        :return:
+        """
+        return
+
+    return 0
