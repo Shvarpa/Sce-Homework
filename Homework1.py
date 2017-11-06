2f4aef09b8d56be615f37f9288493cda5efdf3ea
Pavel Shvarchov <shvarpa@gmail.com>
2017-11-06 17:44:23 +0200

    3

--- Homework1.py
+++ Homework1.py
@@ -4,8 +4,7 @@
 """
 
 
-# import tkinter as tk
-
+# Task 1
 def Nand(x, y):
     """ 
     :param x:
@@ -18,35 +17,100 @@ def Nand(x, y):
     return not (x and y)
 
 
+# Task 2
 def Younger():
     """
     :return: younger name
-    :rtype: string
+    :rtype: person
     """
 
     class date:
         def __init__(self, d=None, m=None, y=None):
+            """
+            :param d: day input
+            :type d: int
+            :param m: month input
+            :type m: int
+            :param y: year input
+            :type y: int
+            """
             self.day = d
             self.month = m
             self.year = y
 
         def __str__(self):
+            """
+            :return: string out of the date parameters
+            :rtype: str
+            """
             return (str(self.day) + '/' + str(self.month) + '/' + str(self.year))
 
         def __lt__(self, other):
+            """
+            :param other:
+            :type other:date
+            :return:
+            :rtype: bool
+            """
             return (self.year < other.year) or (self.year == other.year and (
-            self.month < other.month or (self.month == other.month and self.day < other.day)))
+                self.month < other.month or (self.month == other.month and self.day < other.day)))
 
         def input(self):
-            year = input("Enter year:")
-            month = input("Enter month:")
-            day = input("Enter day:")
+            """
+            :return: updates the date parameters by the user
+            """
+            self.year = input("Enter year:")
+            self.month = input("Enter month:")
+            self.day = input("Enter day:")
 
-    def menu(comment):
-        """
-        :param comment:
-        :return:
-        """
-        return
+    class person:
+        def __init__(self, n):
+            self.bdate = date()
+            self.name = n
 
-    return 0
+        def __str__(self):
+            return str(self.name)
+
+    person1 = person(input("Enter first person name:"))
+    person1.bdate.input()
+    person2 = person(input("Enter second person name:"))
+    person2.bdate.input()
+    if person1.bdate < person2.bdate:
+        print("{} is younger".format(person1))
+        return person1
+    elif person2.bdate < person1.bdate:
+        print("{} is younger".format(person2))
+        return person2
+    else:
+        print("both are the same age")
+        return None
+
+
+# Task 3
+def CheckNumber(x):
+    """
+    :param x:number to check
+    :type x:long, int
+    :return: true if 4*x reversed is the same as x
+    :rtype: bool
+    """
+    def numsize(x):
+        size=0
+        while (x!=0):
+            x=int(x/10)
+            size+=1
+        return size
+
+    def reverse(x):
+        end=numsize(x)-1
+        x=str(x)
+        start=0
+        while (start<end):
+            x[start],x[end]=x[end],x[start]
+            start+=1
+            end-=1
+        return x
+
+    return x==reverse(x*4)
+
+CheckNumber((21978))
